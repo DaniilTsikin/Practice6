deccfee254355e2b4637ed4014af0f3dd915cab9
Daniil <dtsikin@mail.ru>
2022-02-27 18:03:14 +0500

    Complete Task42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Определить сколько чисел больше 0 введено с клавиатуры
+
+int [] arr = new int[10];
+
+void FullArr(int [] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        string? input = Console.ReadLine();
+        int.TryParse(input, out int number);
+        arr[i] = number;
+    }
+}
+
+void ArrPrint(int[] arr)
+{
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}; ");
+    }
+}
+
+int GreaterZero(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        count++;
+    }
+    return count;
+}
+
+
+FullArr(arr);
+ArrPrint(arr);
+int count = GreaterZero(arr);
+Console.Write($"Чисел больше нуля: {count}");
