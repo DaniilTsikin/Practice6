10d3550b62e071d4e7121ade0b67f6ad02567a70
Daniil <dtsikin@mail.ru>
2022-03-01 21:11:23 +0500

    Complete Task46

--- Task46/Program.cs
+++ Task46/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу масштабирования фигуры
+// k = 5
+
+using System.Linq;
+string squareCoordinates = "(20,20) (20,25) (25,20) (25,25)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(squareCoordinates);
+var data = squareCoordinates.Split(" ")
+                .Select(item => item.Split(','))
+                .Select(e => (x: double.Parse(e[0]), y: double.Parse(e[1])))
+                .Select(point => (point.x * 5, point.y * 5))
+                .ToArray();
+for(int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
